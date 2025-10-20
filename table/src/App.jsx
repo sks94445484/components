6c3db3dba7311d22a51d5805295458bcbd19c887
Kubendrasai <sks94445484@gmail.com>
2025-10-20 11:40:35 +0530

    creted sticky table

--- table/src/App.jsx
+++ table/src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import StickyTable from "./stickyTable/StickyTable"
 
 function App() {
 
+  useEffect(()=>{
+    // document.documentElement.style.scroll = "none"
+  },[])
+
   return (
-    <>
+    <div
+      style={{
+        width:"100%",
+        height:"99vh",
+        overflowX:"auto",
+        overflowY:"auto",
+      }}
+    >
+      <div
+        style={{
+          height:"400px",
+          width:"100%",
+        }}
+      >
+
+      </div>
       <StickyTable />
-    </>
+    </div>
   )
 }
 
--- table/src/stickyTable/StickyTable.jsx
+++ table/src/stickyTable/StickyTable.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Table.css";
 
 const StickyTable = () => {
-  const data = Array.from({ length: 50 }, (_, i) => ({
+  const data = Array.from({ length: 500 }, (_, i) => ({
     id: i + 1,
     name: `Name ${i + 1}`,
     age: 20 + (i % 10),
@@ -13,13 +13,23 @@ const StickyTable = () => {
     age1: 20 + (i % 10),
     city1: `City ${i + 1}`,
     country1: `Country ${i + 1}`,
+     id2: i + 2,
+    name2: `Name ${i + 2}`,
+    age2: 20 + (i % 10),
+    city2: `City ${i + 2}`,
+    country2: `Country ${i + 2}`,
+    id3: i + 2,
+    name3: `Name ${i + 2}`,
+    age3: 20 + (i % 10),
+    city3: `City ${i + 2}`,
+    country3: `Country ${i + 2}`,
   }));
 
   return (
     <div className="table-container">
       <table>
         <thead>
-          <tr>
+          <tr className="header">
             <th className="sticky-col left-0 sticky-header" >ID</th>
             <th className="sticky-col left-80 sticky-header" >Name</th>
             <th className="sticky-header">Age</th>
@@ -30,6 +40,16 @@ const StickyTable = () => {
             <th className="sticky-header">Age1</th>
             <th className="sticky-header">City1</th>
             <th className="sticky-header">Country1</th>
+            <th className=" sticky-header">ID2</th>
+            <th className=" sticky-header">Name2</th>
+            <th className="sticky-header">Age2</th>
+            <th className="sticky-header">City2</th>
+            <th className="sticky-header">Country2</th>
+            <th className=" sticky-header">ID3`</th>
+            <th className=" sticky-header">Name3</th>
+            <th className="sticky-header">Age3</th>
+            <th className="sticky-header">City3</th>
+            <th className="sticky-header">Country3</th>
           </tr>
         </thead>
         <tbody>
@@ -53,6 +73,16 @@ const StickyTable = () => {
               <td>{row.age1}</td>
               <td>{row.city1}</td>
               <td>{row.country1}</td>
+              <td >{row.id2}</td>
+              <td >{row.name2}</td>
+              <td>{row.age2}</td>
+              <td>{row.city2}</td>
+              <td>{row.country2}</td>
+              <td >{row.id3}</td>
+              <td >{row.name3}</td>
+              <td>{row.age3}</td>
+              <td>{row.city3}</td>
+              <td>{row.country3}</td>
             </tr>
           ))}
         </tbody>
